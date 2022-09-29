054341d24317fbcc017776b6575469245348a807
Shobola Faizol <shobolafaizol@gmail.com>
2022-09-29 11:31:42 +0100

    Add client section to home page

--- theme/front-page.php
+++ theme/front-page.php
@@ -148,7 +148,7 @@ get_header();
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
         </p>
     </div>
-    <div class="col-span-2 flex flex-col items-center gap-y-4 py-6 border rounded-b transition duration-300 transform bg-white rounded shadow-sm hover:-translate-y-1 hover:shadow md:text-center">
+    <div class="col-span-2 flex flex-col items-center gap-y-4 py-6 border rounded-b transition duration-300 transform bg-white rounded shadow-sm hover:-translate-y-3 hover:shadow md:text-center">
         <div class="flex items-center justify-center w-20 h-20 bg-red-400">
             <svg class="w-16 h-16 text-white lg:w-18 lg:h-18" stroke="currentColor" viewBox="0 0 52 52">
                 <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
@@ -162,7 +162,7 @@ get_header();
             <a href="/" aria-label="" class="inline-flex items-center text-sm md:text-base font-semibold font-body transition-colors duration-200 text-purple hover:text-deep-purple-800">Learn more</a>
         </div>
     </div>
-    <div class="col-span-2 flex flex-col items-center gap-y-4 py-6 border rounded-b transition duration-300 transform bg-white rounded shadow-sm hover:-translate-y-1 hover:shadow md:text-center">
+    <div class="col-span-2 flex flex-col items-center gap-y-4 py-6 border rounded-b transition duration-300 transform bg-white rounded shadow-sm hover:-translate-y-3 hover:shadow md:text-center">
         <div class="flex items-center justify-center w-20 h-20 bg-red-400">
             <svg class="w-16 h-16 text-white lg:w-18 lg:h-18" stroke="currentColor" viewBox="0 0 52 52">
                 <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
@@ -181,7 +181,7 @@ get_header();
 <!-- // SECTION 3 END -->
 <!-- // SECTION 4 -->
 <section class="bg-white">
-  <div class="max-w-screen-xl px-4 py-12 mx-auto md:py-16 sm:px-6 lg:px-8">
+  <div class="cont py-16">
     <div class="max-w-3xl mx-auto text-center">
       <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl">
         Trusted by eCommerce Businesses
@@ -262,5 +262,80 @@ get_header();
   </div>
 </section>
 <!-- // SECTION 4 END -->
+<!-- // SECTION 5 -->
+<section class="bg-white">
+  <div class="cont py-16">
+    <div class="max-w-3xl mx-auto text-center">
+      <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl">
+        Trusted by eCommerce Businesses
+      </h2>
+
+      <p class="mt-4 text-gray-500 sm:text-xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores
+        laborum labore provident impedit esse recusandae facere libero harum
+        sequi.
+      </p>
+    </div>
+
+    <div class="mt-8 sm:mt-12">
+      <dl class="grid grid-cols-1 gap-4 md:gap-0 sm:grid-cols-3">
+        <div class="flex flex-col px-4 py-8 items-center border border-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="w-auto h-36" src="https://thenationonlineng.net/wp-content/uploads/2021/08/dangote-group.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="flex flex-col px-4 py-8 items-center border border-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="w-auto h-36" src="https://www.seekpng.com/png/detail/309-3092375_nbc-plain-logo-1-coca-cola-nigeria-bottling.png" alt="">
+            </div>
+        </div>
+
+        <div class="flex flex-col px-4 py-8 items-center border border-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="w-auto h-36" src="https://w7.pngwing.com/pngs/45/991/png-transparent-cadbury-dairy-milk-logo-brand-food-others-miscellaneous-purple-blue.png" alt="">
+            </div>
+        </div>
+
+        <div class="flex flex-col px-4 py-8 items-center border border-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="w-auto h-36" src="https://www.hotnigerianjobs.com/rimages/seven-up-bottling-company-1604058715-l.png" alt="">
+            </div>
+        </div>
+
+        <div class="flex flex-col px-4 py-8 items-center border border-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="w-auto h-36" src="https://images.africanfinancials.com/ng-uacn-logo.png" alt="">
+            </div>
+        </div>
+
+        <div class="flex flex-col px-4 py-8 items-center border border-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="w-auto h-36" src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1490968716/cik7kf3paqilecmhajm8.png" alt="">
+            </div>
+        </div>
+
+        <div class="flex flex-col px-4 py-8 items-center border border-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="w-auto h-36" src="https://assets.stickpng.com/images/5856a7bb4f6ae202fedf276b.png" alt="">
+            </div>
+        </div>
+
+        <div class="flex flex-col px-4 py-8 items-center border border-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="w-auto h-36" src="https://brandcom.ng/wp-content/uploads/2020/09/BUA-Group.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="flex flex-col px-4 py-8 items-center border border-gray-100">
+            <div class="flex justify-center items-center">
+                <img class="w-auto h-36" src="https://www.kindpng.com/picc/m/350-3504260_lafarge-logo-png-transparent-png.png" alt="">
+            </div>
+        </div>
+      </dl>
+    </div>
+  </div>
+</section>
+<!-- // SECTION 5 END -->
 <?php
 get_footer(); ?>
