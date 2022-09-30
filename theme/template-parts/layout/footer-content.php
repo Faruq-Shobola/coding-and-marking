7d064df55969c4d95bceb04a606f0ae3136e22b5
Shobola Faizol <shobolafaizol@gmail.com>
2022-09-30 13:28:50 +0100

    Redesign and responsive to home page

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -17,72 +17,70 @@
         <!-------------- DETAILS -------------->
         <div class="grid grid-col-1 md:grid-cols-6 gap-y-8">
             <!-------------- COLUMN 1 -------------->
-                <div class="md:col-span-2">
-                    <h3 class="text-2xl font-bold capitalize text-red-400">
-                    zenith precision
-                    </h3>
-                
-                    <p class="mt-4 text-lg text-gray font-medium">
-                        Zenith precision ltd offers integrated coding and marking solutions focusing the manufacturing industry.
-                    </p>
-
-                    
-                </div>
+			<div class="md:col-span-2">
+				<h3 class="text-2xl font-bold capitalize text-whine">
+				zenith precision
+				</h3>
+			
+				<p class="mt-4 text-base text-gray md:text-lg">
+					Zenith precision ltd offers integrated coding and marking solutions focusing the manufacturing industry.
+				</p>                    
+			</div>
             <!-------------- COLUMN 2 -------------->
-                <div class="md:col-span-1 md:flex md:justify-center">
-                    <ul class="text-gray font-medium">
-                        <li class="capitalize pb-2"><a href="index.html">home</a></li>
-                        <li class="capitalize pb-2"><a href="about.html">about us</a></li>
-                        <li class="capitalize pb-2"><a href="services.html">services</a></li>
-                        <li class="capitalize"><a href="contact.html">contact us</a></li>
-                    </ul>
-                </div>
+			<div class="md:col-span-2 lg:col-span-1 md:flex md:justify-center">
+				<ul class="text-base text-gray md:text-lg">
+					<li class="capitalize pb-2"><a href="index.html">home</a></li>
+					<li class="capitalize pb-2"><a href="about.html">about us</a></li>
+					<li class="capitalize pb-2"><a href="services.html">services</a></li>
+					<li class="capitalize"><a href="contact.html">contact us</a></li>
+				</ul>
+			</div>
             <!-------------- COLUMN 3 -------------->
-                <div class="md:col-span-1 md:flex md:justify-center">
-                    <ul class="text-gray font-medium">
-                        <li class="capitalize pb-2"><a href="blog.html">blog</a></li>
-                        <li class="capitalize pb-2"><a href="about.html#faq">FAQs</a></li>
-                        <li class="capitalize"><a href="appointment.html">book an appointment</a></li>
-                    </ul>
-                </div>
+			<div class="md:col-span-2 lg:col-span-1 md:flex md:justify-center">
+				<ul class="text-base text-gray md:text-lg">
+					<li class="capitalize pb-2"><a href="blog.html">blog</a></li>
+					<li class="capitalize pb-2"><a href="about.html#faq">FAQs</a></li>
+					<li class="capitalize"><a href="appointment.html">book an appointment</a></li>
+				</ul>
+			</div>
             <!-------------- COLUMN 4 -------------->
-                <div class="relative md:col-span-2 flex flex-col items-start md:items-end  justify-end md:text-right gap-4">
-                    <!-------------- BACK TO TOP -------------->
-                    <div class="fixed bottom-14 right-10 md:bottom-20 md:right-12">
-                        <div class="flex justify-center items-center h-12 w-12 bg-red-400">
-                            <i class="fas fa-angle-up text-3xl text-white"></i>
-                        </div>
-                    </div>
-                    <!-------------- SOCIALS -------------->
-                    <div class="flex justify-center items-center gap-4">
-                        <div class="flex justify-center items-center h-8 w-8 bg-red-400 rounded-full">
-                            <i class="fab fa-facebook-f text-white"></i>
-                        </div>
-                        <div class="flex justify-center items-center h-8 w-8 bg-red-400 rounded-full">
-                            <i class="fab fa-twitter text-white"></i>
-                        </div>
-                        <div class="flex justify-center items-center h-8 w-8 bg-red-400 rounded-full">
-                            <i class="fab fa-instagram text-white"></i>
-                        </div>
-                        <div class="flex justify-center items-center h-8 w-8 bg-red-400 rounded-full">
-                            <a href="https://www.linkedin.com/company/zenith-precision-limited/"><i class="fab fa-linkedin-in text-white"></i></a>
-                        </div>
-                    </div>
-                    <!-------------- GET IN TOUCH-------------->
-                    <div class="heading">
-                        <p class="text-xl font-medium">
-                            Get in Touch
-                        </p>
-                        <a href="mailto:hi@zenithprecision.com.ng" class="text-xl font-medium text-red-400">
-                            hi@zenithprecision.com.ng
-                        </a>
-                    </div>
-                </div>
+			<div class="relative md:col-span-6 lg:col-span-2 flex flex-col items-start md:items-start lg:items-end justify-end md:text-left lg:text-right gap-4">
+				<!-------------- BACK TO TOP -------------->
+				<div class="fixed bottom-14 right-10 md:bottom-20 md:right-12">
+					<div class="flex justify-center items-center h-12 w-12 bg-whine">
+						<i class="fas fa-angle-up text-3xl text-white"></i>
+					</div>
+				</div>
+				<!-------------- SOCIALS -------------->
+				<div class="flex justify-center items-center gap-4">
+					<div class="flex justify-center items-center h-8 w-8 bg-whine rounded-full">
+						<i class="fab fa-facebook-f text-white"></i>
+					</div>
+					<div class="flex justify-center items-center h-8 w-8 bg-whine rounded-full">
+						<i class="fab fa-twitter text-white"></i>
+					</div>
+					<div class="flex justify-center items-center h-8 w-8 bg-whine rounded-full">
+						<i class="fab fa-instagram text-white"></i>
+					</div>
+					<div class="flex justify-center items-center h-8 w-8 bg-whine rounded-full">
+						<a href="https://www.linkedin.com/company/zenith-precision-limited/"><i class="fab fa-linkedin-in text-white"></i></a>
+					</div>
+				</div>
+				<!-------------- GET IN TOUCH-------------->
+				<div class="heading">
+					<p class="text-base text-gray md:text-lg">
+						Get in Touch
+					</p>
+					<a href="mailto:hi@zenithprecision.com.ng" class="text-base md:text-lg text-whine">
+						hi@zenithprecision.com.ng
+					</a>
+				</div>
+			</div>
         </div>  
         <!-------------- COPYRIGHT - TERMS & CONDITION -------------->
         <div class="copyright justify-self-start flex flex-wrap sm:flex-nowrap gap-x-6">
-            <p class="capitalize text-base font-medium text-gray">copyright © 2021 zenith precision . all rights reserved.</p>
-            <a href="#" class="capitalize text-red-400 text-base font-medium cursor-pointer">Terms &amp; Condition</a>
+            <p class="capitalize text-base text-gray">copyright © 2021 zenith precision. all rights reserved.</p>
+            <a href="#" class="capitalize text-whine text-base cursor-pointer">Terms &amp; Condition</a>
         </div>
             
     </footer>
