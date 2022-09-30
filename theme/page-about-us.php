8c748d59f72bdc94b34ab845a96542e90f9ef4e2
shobola faruq <shobolafaruq@gmail.com>
2022-09-30 12:11:11 +0100

    Add about section on about-us page

--- theme/page-about-us.php
+++ theme/page-about-us.php
@@ -22,7 +22,7 @@ get_header();
                 </h2>
                 <div class="w-32 h-1 bg-red-600 mx-auto my-6 "></div>
                 <p class="mb-4 text-base text-gray-400 md:text-lg">
-                    Zenith pricision Ltd is a coding and marking company in Nigeria, incoporated in year 2007 and commence coding and marking business in 2012. it's goal is to be the preferred coding and marking solution provider delivering excellent customer experience as there are rules and regulations by various global and international laws, standard and guideline to provide essential product information
+                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, consequatur?
                 </p>
                 <a href="/" aria-label="" class="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-red-400 hover:text-red-700">
                 Learn more
@@ -35,6 +35,33 @@ get_header();
         </div>
     </div>
 </div>
+<!--  -->
+<section class="">
+    <div class="cont flex flex-col justify-between py-8 xl:grid grid-cols-2 gap-4 md:px-8 md:py-16">
+        <div class="flex flex-col justify-center lg:ml-7 gap-y-7">
+            <div class="">
+                <!-- <h2 class="mb-6 text-4xl md:text-5xl capitalize font-bold font-head tracking-tight text-red-400 sm:leading-none">
+                    Manage Everything in Your Hand
+                </h2> -->
+                <p class="text-base text-gray md:text-base py-3 font-body">
+                    Zenith pricision Ltd is a coding and marking company in Nigeria, incoporated in year 2007 and commence coding and marking business in 2012. it's goal is to be the preferred coding and marking solution provider delivering excellent customer experience as there are rules and regulations by various global and international laws, standard and guideline to provide essential product information.
+                </p>
+                <p class="text-base text-gray md:text-base py-3 font-body">
+                    Coding and marking ensure traceability, safety and authencity of products, promote brand positivity, avoid counterfieting and provide regulatory requirement.
+                </p>
+                <p class="text-base text-gray md:text-base py-3 font-body">
+                    Zenith precision mission is to provide world class coding and marking solution leveeaging technology to meet customers and stakeholders' expectations.
+                </p>
+                <p class="text-base text-gray md:text-base py-3 font-body">
+                    Zenith precision existing clients are Nigerian Bottling Company, Dangote Sugar Refinery, Unilever Plc, Lafarge Cement, Juhel Pharmaceutical Enugu, BUA Sugar Refinery, Chi LImited, Ashaka Cement Gombe and Anochemical Cosmetics Limited Agbara.
+                </p>
+            </div>
+        </div>
+        <div class="slide-right">
+            <img src="http://test.zenithprecision.com.ng/Images/Zenith_logo+.png" class="object-cover object-top w-full h-auto mx-auto xl:mr-24 md:max-w-xl" alt="" />
+        </div>
+    </div>
+</section>
 
 <?php
 get_footer(); ?>
