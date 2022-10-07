519684efd600feee50915db1a037264e6f5152cc
shobola faruq <shobolafaruq@gmail.com>
2022-10-07 08:07:42 +0100

    Add business-branch section on branch page

--- theme/page-branch.php
+++ theme/page-branch.php
@@ -18,7 +18,7 @@ get_header();
         <div class="flex flex-col items-center justify-between">
             <div class="relative w-full mb-12 xl:mb-0 xl:pr-16 xl:w-7/12 bg-black bg-opacity-40 p-16 text-center">
                 <h2 class="font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none capitalize">
-               who we are
+               our branch
                 </h2>
                 <div class="w-32 h-1 bg-red-600 mx-auto my-6 "></div>
                 <p class="mb-4 text-base text-gray-400 md:text-lg">
@@ -35,6 +35,65 @@ get_header();
         </div>
     </div>
 </div>
+<!--  -->
+<section class="py-10">
+    <div class="cont flex flex-col justify-between py-16 md:grid grid-cols-2 gap-8">
+        <div class="flex flex-col justify-center gap-y-7">
+            <div class="">
+                <div class="flex justify-between items-center py-6">
+                    <div class="w-3/5">
+                        <h4 class="uppercase text-whine font-semibold text-lg">Address</h4>
+                    </div>
+                    <div class="w-2/5">
+                        <h4 class="uppercase text-whine font-semibold text-lg">sales inquiries</h4>
+                    </div>
+                </div>
+
+                <div class="flex justify-between items-center py-6 border-t-2">
+                    <div class="w-3/5">
+                        <div class="flex flex-col w-4/5">
+                            <span class="uppercase font-semibold text-xs text-gray-500 tracking-widest pb-3">head office</span>
+                            <span class="font-medium text-base leading-relaxed">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</span>
+                        </div>
+                    </div>
+                    <div class="w-2/5">
+                        <span class="font-medium text-base text-gray-500 leading-relaxed">(Tel)<a href="tel:+2345678900983" class="font-medium text-base text-black leading-relaxed">+2345678900983</a></span>
+                    </div>
+                </div>
+
+                
+                <div class="flex justify-between items-center py-6 border-t-2">
+                    <div class="w-3/5">
+                        <div class="flex flex-col w-4/5">
+                            <span class="uppercase font-semibold text-xs text-gray-500 tracking-widest pb-3">kano, branch office</span>
+                            <span class="font-medium text-base leading-relaxed">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</span>
+                        </div>
+                    </div>
+                    <div class="w-2/5">
+                        <span class="font-medium text-base text-gray-500 leading-relaxed">(Tel)<a href="tel:+2345678900983" class="font-medium text-base text-black leading-relaxed">+2345678900983</a></span>
+                    </div>
+                </div>
+
+                
+                <div class="flex justify-between items-center py-6 border-t-2">
+                    <div class="w-3/5">
+                        <div class="flex flex-col w-4/5">
+                            <span class="uppercase font-semibold text-xs text-gray-500 tracking-widest pb-3">head office</span>
+                            <span class="font-medium text-base leading-relaxed">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</span>
+                        </div>
+                    </div>
+                    <div class="w-2/5">
+                        <span class="font-medium text-base text-gray-500 leading-relaxed">(Tel)<a href="tel:+2345678900983" class="font-medium text-base text-black leading-relaxed">+2345678900983</a></span>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="slide-right flex justify-center items-center border border-1">
+            <img src="http://test.zenithprecision.com.ng/Images/Zenith_logo+.png" class="w-auto h-80" alt="" />
+        </div>
+    </div>
+</section>
 
 <?php
 get_footer(); ?>
