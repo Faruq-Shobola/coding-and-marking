2e4f3b0100a60dfa75a56397432ec5e1991b42c7
Shobola Faizol <shobolafaizol@gmail.com>
2022-10-07 17:16:08 +0100

    Modify breadcrumb across pages

--- theme/header.php
+++ theme/header.php
@@ -15,6 +15,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700&display=swap" rel="stylesheet">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
--- theme/page-contact-us.php
+++ theme/page-contact-us.php
@@ -36,17 +36,17 @@ get_header();
     <section class="cont text-gray-600 body-font relative">
         <div class="container py-24 mx-auto flex flex-col lg:grid grid-cols-2 gap-8">
             <div class=" bg-gray-300 overflow-hidden py-44 md:p-10 md:pt-72 lg:p-5 flex items-end justify-start relative">
-                <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"></iframe>
-                <div class="bg-white relative hidden md:flex flex-wrap py-6 shadow-sm ">
+                <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://www.google.com/maps/embed/v1/place?q=12+Oladele+Kadiri+Close,+Agege,+Nigeria&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
+                <div class="bg-white relative hidden md:flex flex-wrap py-6 shadow-sm text-sm">
                     <div class="lg:w-1/2 px-6">
-                        <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-                        <p class="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
+                        <h2 class="title-font font-semibold text-gray-900 tracking-widest">ADDRESS</h2>
+                        <p class="mt-1">Plot 12, Oladele Kadiri close Off Nob-Oluwa Street, Ogba, Ikeja Lagos.</p>
                     </div>
                     <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
                         <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-                        <a class="text-indigo-500 leading-relaxed">example@email.com</a>
+                        <a href="mailto:hi@zenithprecision.com.ng" class="text-whine leading-relaxed">hi@zenithprecision.com.ng</a>
                         <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-                        <p class="leading-relaxed">123-456-7890</p>
+                        <a href="tel:+2348081989240" class="leading-relaxed text-whine">+2348 081 989 240</a>
                     </div>
                 </div>
             </div>
